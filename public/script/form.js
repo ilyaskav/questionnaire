4844dbf3fbb29d8b90e8dc4cf449b66fa01c063e
Ilya Skavronskiy <ilya.skav@gmail.com>
2017-06-29 18:14:42 +0300

    added alert on success form submission

--- public/script/form.js
+++ public/script/form.js
@@ -4,11 +4,15 @@ $(document).ready(function () {
         document.getElementById("codeQualitySliderVal").textContent = sliderValue;
     });
 
-    $('#questionnaireForm').on('submit', function(){
-        var formData = $(this).serialize();
+    $('#questionnaireForm').on('submit', function(e){
+        e.preventDefault();
+        var form = $('#questionnaireForm');
+        var formData = form.serialize();
         
-        $.post ('/submitForm', formData)
+        $.post ('/submitForm', formData, 'json')
             .done(function(){
+                $('.alert').show();
+                form[0].reset();
                 console.log('request has been successfully sent')
             })
             .fail(function(){
