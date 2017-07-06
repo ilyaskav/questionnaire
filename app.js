141d7605d073a9825f3c53f06564668cf34c3b29
Ilya Skavronskiy <ilya.skav@gmail.com>
2017-07-06 23:25:23 +0300

    learning datatables

--- app.js
+++ app.js
@@ -17,14 +17,14 @@ app.set('view engine', 'ejs');
 // middleware
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(expressValidator()); // this line must be immediately after any of the bodyParser middlewares! 
-app.use(express.static( 'public'));
+app.use(express.static('public'));
 
 app.get('/', function (request, response) {
-  response.render('index', {title: 'Brandply questionnaire'});
+  response.render('index', { title: 'Brandply questionnaire' });
 });
 
 app.get('/form', function (request, response) {
-  response.render('form', {title: 'Brandply questionnaire: answer the questions'});
+  response.render('form', { title: 'Brandply questionnaire: answer the questions' });
 });
 
 app.post('/submitForm', function (req, res) {
@@ -40,11 +40,11 @@ app.post('/submitForm', function (req, res) {
       return;
     }
 
-    req.body.date=new Date();
+    req.body.date = new Date();
 
     fileIO.writeObj(req.body).then(
       function (result) {
-        res.json({result: result});
+        res.json({ result: result });
       },
       function (result) {
         res.status(400).send(result);
@@ -58,14 +58,14 @@ app.get('/results', function (req, res) {
   fileIO.readObj().then(
     function (data) {  // успех
 
-      data.forEach(function(item, i, data){
-        item.date=moment(item.date).format('MMMM Do YYYY, h:mm:ss a');
+      data.forEach(function (item, i, data) {
+        item.date = moment(item.date).format('MMMM Do YYYY, h:mm:ss a');
       });
 
       var locals = {
         title: 'Brandply questionnaire: results',
         data: data,
-        error: data.length? null : "No questionnaire answers yet."
+        error: data.length ? null : "No questionnaire answers yet."
       }
       res.render('results', locals);
     },
@@ -74,6 +74,25 @@ app.get('/results', function (req, res) {
     });
 });
 
+app.get('/datatables', function (req, res) {
+  res.render('datatables', { title: 'Datatables' });
+});
+
+app.get('/getData', function (req, res) {
+  var data = {
+    "data": [
+      {
+        "user": "ilya",
+        "email": "ilya@gmail.com"
+      },
+      {
+        "user": "Olya",
+        "email": "olya@gmail.com"
+      }]
+  };
+
+  res.json(data);
+});
 
 var port = 8080;
 app.listen(port);
